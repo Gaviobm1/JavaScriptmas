cb3e3ac06cbd9a0db7486752e755e74fcaef5127
Gavin O Brien <142741214+Gaviobm1@users.noreply.github.com>
2023-12-02 12:32:13 +0100

    Add first two projects of code challenge

---
+++ CountdownTimer/countDown.js
@@ -0,0 +1,28 @@
+setInterval(function renderCountdown(){
+    const christmas = new Date(2023, 11, 25);
+    const now = new Date();
+    const diff = christmas.getTime() - now.getTime();
+    
+    const totalSecs = Math.floor(diff / 1000);
+    const totalMins = Math.floor(totalSecs / 60);
+    const totalHours = Math.floor(totalMins / 60);
+    const totalDays = Math.floor(totalHours / 24);
+    const timer = document.getElementById("countdown-display");
+        
+    
+    const hours = totalHours % 24;
+    const minutes = totalMins % 60;
+    const seconds = totalSecs % 60;
+    
+    function addZeros(unit) {
+        if (unit < 10) {
+            return `0${unit}`
+        }
+        return unit
+    }
+        
+    timer.textContent = `${addZeros(totalDays)}:${addZeros(hours)}:${addZeros(minutes)}:${addZeros(seconds)}`
+}, 1000)
+
+ 
+renderCountdown()
